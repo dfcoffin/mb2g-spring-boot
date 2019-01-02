990c1bb590ac089a9156f49a4513aa18750d9acf
Donald F. Coffin <donald.coffin@reminetworks.com>
2019-01-01 20:24:27 -0500

    Adding controller for fat jar example

---
+++ src/main/java/guru/springframework/springboot/controllers/ExampleController.java
@@ -0,0 +1,23 @@
+package guru.springframework.springboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Donald F. Coffin on 2019-01-01 at 19:42
+ **/
+@RestController
+public class ExampleController {
+
+    @RequestMapping("/")
+    public Map<String , Object> getIndex() {
+        Map<String, Object> returnval = new HashMap<>();
+
+        returnval.put("hello", "world");
+
+        return returnval;
+    }
+}
